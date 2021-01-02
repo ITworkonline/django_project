2beb5954cfce1b9ca9c64c2689df8c9944b88a10
ITworkonline <1130172267@qq.com>
2021-01-01 22:45:23 -0700

    update class based api view

--- MyProject/api_basic/urls.py
+++ MyProject/api_basic/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
 urlpatterns = [
-    path('article/', article_list),
+    #path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+
 ]
